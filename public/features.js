22c63928309b938e37bca21af3e6f9623dfeaaa8
Hector Guerrero <hectorguerrero1866@gmail.com>
2024-04-14 18:37:52 -0400

    now fe is working as espected.

--- public/features.js
+++ public/features.js
@@ -1,35 +1,58 @@
 let app = angular.module('featuresApp', []);
 
-app.controller('ItemsCtrl', function($scope, $http) {
-  $scope.items = [];
-  $scope.currentPage = 1;
-  $scope.totalPages = 1;
-  $scope.perPage = 10;
+app.controller('ItemsCtrl', function ($scope, $http) {
+    $scope.items = [];
+    $scope.currentPage = 1;
+    $scope.totalPages = 1;
+    $scope.perPage = 5;
+    $scope.magType = 'all';
+    $scope.error = null
 
-  function fetchData(page,per_page) {
-    $http.get('/api/features', { params: { page: page ,per_page: per_page} })
-      .then(function(response) {
-        $scope.items = response.data.data;
-        $scope.totalPages = Math.ceil(response.data.pagination.total / per_page);
-      })
-      .catch(function(error) {
-        console.error(error);
-      });
-  }
+    function fetchData(page, per_page, mag_type) {
+        $http.get('/api/features', {params: {page: page, per_page: per_page, mag_type: mag_type}})
+            .then(function (response) {
+                $scope.items = response.data.data;
+                $scope.totalPages = response.data.pagination.total;
+                console.log($scope.items);
+                if ($scope.items.length === 0) {
+                    $scope.currentPage = 1;
+                    $scope.totalPages = 1;
+                    $scope.perPage = 5;
+                    $scope.error = "Error there is no data with that magnitude type. Try another";
+                    // fetchData($scope.currentPage, $scope.perPage);
+                }else
+                {
+                    $scope.error = null
+                }
 
-  fetchData($scope.currentPage,$scope.perPage);
+            })
+            .catch(function (error) {
+                console.error(error);
+                $scope.error = `Error: ${error.message}`;
 
-  $scope.nextPage = function() {
-    if ($scope.currentPage < $scope.totalPages) {
-      $scope.currentPage++;
-      fetchData($scope.currentPage,$scope.perPage);
+            });
     }
-  };
 
-  $scope.prevPage = function() {
-    if ($scope.currentPage > 1) {
-      $scope.currentPage--;
-      fetchData($scope.currentPage,$scope.perPage);
-    }
-  };
+    fetchData($scope.currentPage, $scope.perPage);
+
+    $scope.filterByMagType = function () {
+        $scope.currentPage = 1;
+        $scope.totalPages = 1;
+        $scope.perPage = 5;
+        fetchData($scope.currentPage, $scope.perPage, $scope.magType);
+    };
+
+    $scope.nextPage = function () {
+        if ($scope.currentPage < $scope.totalPages) {
+            $scope.currentPage++;
+            fetchData($scope.currentPage, $scope.perPage, $scope.magType);
+        }
+    };
+
+    $scope.prevPage = function () {
+        if ($scope.currentPage > 1) {
+            $scope.currentPage--;
+            fetchData($scope.currentPage, $scope.perPage,$scope.magType);
+        }
+    };
 });
