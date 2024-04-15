579117fa723003834e2f401ff66a0bd764f2494d
Hector Guerrero <hectorguerrero1866@gmail.com>
2024-04-14 20:07:17 -0400

    adding more content to the .sql file.

--- features.sql
+++ features.sql
@@ -1,35 +1,39 @@
--- we don't know how to generate root <with-no-name> (class Root) :(
-create table features
+create database earthquake
+    with owner postgres;
+
+create table public.features
 (
     external_id varchar not null
         constraint features_pk_2
             unique,
-    id serial not null
+    id          serial
         constraint features_pk
             primary key,
-    magnitude numeric,
-    place varchar not null,
-    time varchar,
-    tsunami boolean,
-    magtype varchar not null,
-    title varchar not null,
-    longitude numeric,
-    latitude numeric,
-    url varchar not null
+    magnitude   numeric,
+    place       varchar not null,
+    time        varchar,
+    tsunami     boolean,
+    magtype     varchar not null,
+    title       varchar not null,
+    longitude   numeric,
+    latitude    numeric,
+    url         varchar not null
 );
 
-alter table features owner to postgres;
+alter table public.features
+    owner to postgres;
 
-create table comments
+create table public.comments
 (
-    id serial not null
+    id          serial
         constraint comments_pk
             primary key,
-    text text not null,
+    text        text    not null,
     feauture_id integer not null
         constraint comments_features_id_fk
-            references features
+            references public.features
             deferrable initially deferred
 );
 
-alter table comments owner to postgres;
+alter table public.comments
+    owner to postgres;
